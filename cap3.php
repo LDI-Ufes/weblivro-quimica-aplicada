74fc2d4c72533e983a699d3308cc7ad3bdfae664
Isabela Oliveira <oliver.isabela2303@gmail.com>
2021-12-01 11:51:01 -0300

    visualização dos boxes das fórmulas
    adicionei a numeração respectiva a cada fórmula; e setei tamanhos diferentes de largura mínima para telas menores no css para visualização com barra de scroll

--- cap3.php
+++ cap3.php
@@ -32,9 +32,15 @@ include("components/page-header.php");
 <p>Do ponto de vista cerâmico, as argilas são plásticas e moldáveis quando suficientemente fragmentadas e úmidas. Por outro lado, são rígidas quando secas, e vitrosas quando queimadas em temperaturas elevadas. Em quase todas as argilas usadas na indústria de cerâmica, o mineral básico é a caulinita, embora às vezes se use a bentonita, cuja base é a montmorillonita, quando se deseja uma maior plasticidade. Como as argilas apresentam uma grande variação de suas propriedades físicas e na quantidade de impurezas (quantidades variáveis de feldspato, quartzo, óxidos de ferro, etc.) que contém, é necessário realizar um processo de beneficiamento das mesmas.</p>
 <p>Geralmente, esse beneficiamento refere-se a operações unitárias de natureza física, tais como a separação granulométrica por peneiramento ou decantação seletiva, filtração e secagem. A purificação química também pode ser usada para produzir materiais de elevada pureza, como a alumina e o óxido de titânio.</p>
 <p>O feldspato é o principal fundente empregado nas formulações cerâmicas. Ele pode estar presente na argila da jazida, ou pode ser adicionado conforme a necessidade. Os três tipo mais comuns de feldspatos usados na produção de produtos cerâmicos são os potássicos \((K_2O·Al_2O_3·6SiO_2)\), os sódicos \((Na_2O·Al_2O_3·6SiO_2)\) e os cálcicos \((CaO·Al_2O_3·6SiO_2)\). A seguir, tem-se a reação química de formação da caulinita a partir de um feldspato potássico:</p>
+
 <!-- (isabela) coloquei fórmula por enquanto -->
-<div class = "box-centro">
-    <p>\(K_2O·Al_2O_3·6SiO_2 + CO_2 + 2H_2O \rightarrow\) \(K_2CO_3 + Al_2O_3·2SiO_2·2H_2O + 4SiO_2\)</p>
+<div class="box-centro">
+    <div class="formula longa">
+        <div class="line">
+            <p>\(K_2O·Al_2O_3·6SiO_2 + CO_2 + 2H_2O \rightarrow\) \(K_2CO_3 + Al_2O_3·2SiO_2·2H_2O + 4SiO_2\)</p>
+            <span class="box-eq">equação 3.1</span>
+        </div>
+    </div>
 </div>
 
 <p>Por fim, o terceiro constituinte principal dos produtos cerâmicos é a areia ou sílica \((SiO_2)\). Segundo a NBR 7211/83, a areia divide-se, granulometricamente, em:</p>
@@ -62,15 +68,25 @@ include("components/page-header.php");
 <p>O primeiro efeito do calor é a eliminação da água de hidratação, que ocorre nas temperaturas entre 600 e 650 ºC. Esse processo é bastante endotérmico e gera um resíduo amorfo de alumina e sílica:</p>
 <!-- ESTEQUIOMETRIA 3.2 SERÁ IMAGEM OU CÓDIGO? -->
 <!-- (isabela) coloquei fórmula por enquanto -->
-<div class = "box-centro">
-    <p>\(Al_2O_3·2SiO_2·2H_2O \rightarrow Al_2O_3 + 2SiO_2\) \( + 2H_2O\)</p>
+<div class="box-centro">
+    <div class="formula pequena">
+        <div class="line">
+            <p>\(Al_2O_3·2SiO_2·2H_2O \rightarrow Al_2O_3 + 2SiO_2\) \( + 2H_2O\)</p>
+            <span class="box-eq">equação 3.2</span>
+        </div>
+    </div>
 </div>
 
 <p>O aumento da temperatura (940 ºC) promove uma transformação da alumina amorfa em uma forma cristalina conhecida como γ-alumina, sendo esse segundo processo exotérmico. Em seguida, um pequeno aumento da temperatura, começando em torno de 1000 ºC promove a combinação da sílica com a alumina para formar a mulita \((3Al_2O_3·2SiO_2)\). A uma temperatura ainda mais alta, a sílica remanescente é convertida a cristobalita cristalina. A reação química global para o aquecimento da argila é representada na eq. 3.3.</p>
 <!-- ESTEQUIOMETRIA 3.3 SERÁ IMAGEM OU CÓDIGO? -->
 <!-- (isabela) coloquei fórmula por enquanto -->
-<div class = "box-centro">
-    <p>\(3(Al_2O_3·2SiO_2·2H_2O) \rightarrow\) \(3Al_2O_3·2SiO_2 + 4SiO_2 + 6H_2O\)</p>
+<div class="box-centro">
+    <div class="formula media">
+        <div class="line">
+            <p>\(3(Al_2O_3·2SiO_2·2H_2O) \rightarrow\) \(3Al_2O_3·2SiO_2 + 4SiO_2 + 6H_2O\)</p>
+            <span class="box-eq">equação 3.3</span>
+        </div>
+    </div>
 </div>
 
 <p>A presença de compostos fundentes tende a baixar a temperatura de formação da mulita.</p>
@@ -83,8 +99,9 @@ include("components/page-header.php");
     <li> Vitrificação completa: esmaltes e vidros.</li>
 </ul>
 
-<br><div class="atv">
-    <h3 class = "atividade">Atividade 3.1. </h3>
+<br>
+<div class="atv">
+    <h3 class="atividade">Atividade 3.1. </h3>
     <p>Fazer um fluxograma simplificado do processo de aquecimento da caulinita. </p>
 </div>
 
@@ -108,7 +125,7 @@ include("components/page-header.php");
 <p>Em seguida, tem-se o processo de moagem, mais propriamente, moagem do “cru” que consiste na redução a pó da mistura de matérias primas, fazendo com que a dimensão dos grânulos fique compreendida entre 3 e 30 mm. Simultaneamente à moagem ocorre um processo de adição de outros materiais: areia \((SiO_2)\), cinzas de pirita \((Fe_2S)\), e bauxite \((Al_2O_3)\), de forma a obter as quantidades pretendidas dos compostos que constituem o "cru": cálcio, sílica, alumínio e ferro, essenciais para a fabricação do cimento. A moagem do “cru” pode ser realizada em um moinho de bolas. Os silos de armazenagem do material moído e da farinha crua também têm a função de homogeneização, que é muito importante para a economia de energia e um fator determinante para qualidade do produto final.</p>
 <p>Depois da moagem do “cru” vem à operação de cozedura ou clinquerização, que origina, por reações químicas complexas, um produto granulado denominado clínquer. Antes do “cru” dar entrada no forno, ele tem que ser progressivamente aquecido até à temperatura de 850-900 ºC. Este processo de aquecimento ocorre na torre de ciclones que é constituída por vários ciclones sobrepostas verticalmente, em que cada um, é constituído por um cilindro e um cone na parte inferior, conforme esquematizado no desenho da Figura 3.2. O ar quente e poeiras provenientes do forno entram pela parte superior do ciclone inferior segundo uma tangente ao cilindro. Devido à configuração do ciclone, as partículas precipitam através da parte inferior enquanto que o ar quente parcialmente purificado escapa-se pela parte superior, passando ao ciclone imediatamente acima onde ocorre o mesmo processo até que, no último ciclone, o gás é liberado a uma temperatura de 900 ºC arrastando consigo, uma quantidade mínima de poeiras encaminhado-as para os eletrofiltros.</p>
 <!-- FIGURA 3.2 -->
-<figure class = "lateral">
+<figure class="lateral">
     <img src="img/fig-3-2.jpg" alt="audiodescrição">
     <figcaption><span class="bold">Figura 3.2.</span>Esquema de funcionamento de uma torre de ciclones.</figcaption>
 </figure>
@@ -121,18 +138,33 @@ include("components/page-header.php");
 <p>Basicamente, todas as conversões químicas envolvidas na produção do cimento, estão presentes na etapa de clinquerização, como podemos observar nas etapas apresentadas a seguir: </p>
 <p><span class="bold">I. Evaporação da água livre:</span> ocorre no primeiro estágio de ciclones em temperaturas abaixo de 100 °C. </p>
 <div class="box-centro">
-    <p>\(H_2O(l) \rightarrow H_2O(g)\) 539,6 cal g<sup>-1</sup></p>
+    <div class="formula">
+        <div class="line">
+            <p>\(H_2O(l) \rightarrow H_2O(g)\) 539,6 cal g<sup>-1</sup></p>
+            <span class="box-eq">equação 3.4</span>
+        </div>
+    </div>
 </div>
 
 <p><span class="bold">II. Decomposição do carbonato de magnésio:</span> a decomposição da dolomita em \(MgO\) e \(CO_2\) tem início em 340 °C, porém a medida que o teor de cálcio aumenta, também se eleva a temperatura de decomposição.</p>
 <div class="box-centro">
-    <p>\(MgCO_3(s) \rightarrow MgO(s) + CO_2(g)\) 270 cal g<sup>-1</sup> </p>
+    <div class="formula">
+        <div class="line">
+            <p>\(MgCO_3(s) \rightarrow MgO(s) + CO_2(g)\) 270 cal g<sup>-1</sup> </p>
+            <span class="box-eq">equação 3.5</span>
+        </div>
+    </div>
 </div>
 <p>O \(MgO\) liberado vai dissolver-se na fase líquida (fundida), formada durante a queima e em parte formará soluções sólidas com as fases mais importantes do clínquer. Na temperatura de clinquerização o \(MgO\) não se combina com os demais óxidos presentes, ficando livre na forma de periclásio.</p>
 
 <p><span class="bold">III. Decomposição do carbonato de cálcio: </span>esta reação tem início em temperatura acima de 805 °C, sendo 894 °C a temperatura crítica de dissociação do carbonato de cálcio puro a 1 atm de pressão.</p>
 <div class="box-centro">
-    <p>\(CaCO_3(s) \rightarrow CaO + CO_2(g)\) 393 cal g<sup>-1</sup> </p>
+    <div class="formula">
+        <div class="line">
+            <p>\(CaCO_3(s) \rightarrow CaO + CO_2(g)\) 393 cal g<sup>-1</sup> </p>
+            <span class="box-eq">equação 3.6</span>
+        </div>
+    </div>
 </div>
 <p>Esta reação química que ocorre em <span class="bold">III</span>, conhecida como descarbonatação é uma das principais reações de obtenção do clínquer, devido ao grande consumo de energia que é necessário à sua realização e à influência sobre a velocidade de deslocamento de material no forno. Nos fornos com pré-calcinadores cerca de 94% da descarbonatação ocorre no pré-calcinador e o restante no forno. Em fornos sem pré-calcinadores cerca 60% ocorre nos ciclones. É imprescindível que a descarbonatação esteja completa para que o material penetre na zona de alta temperatura no forno (zona de clinquerização).</p>
 
@@ -140,17 +172,32 @@ include("components/page-header.php");
 
 <p><span class="bold">V. Formação do silicato dicálcico \((2CaO·SiO_2)\): </span>tem início em temperatura de 900 °C onde mesmo sílica livre e \(CaO\) já reagem lentamente. Na presença de ferro e alumínio esta reação química é acelerada.</p>
 <div class="box-centro">
-    <p>\(2CaO + SiO_2 \rightarrow\) \(2CaO·SiO_2\)</p>
+    <div class="formula">
+        <div class="line">
+            <p>\(2CaO + SiO_2 \rightarrow\) \(2CaO·SiO_2\)</p>
+            <span class="box-eq">equação 3.7</span>
+        </div>
+    </div>
 </div>
 
 <p><span class="bold">VI. Formação do silicato tricálcico \((3CaO·SiO_2)\): </span>inicia sua formação entre 1200 °C e 1300 °C a 1400 °C os produtos de reação são \(3CaO·SiO_2\), \(2CaO·SiO_2\), \(3CaO·Al_2O_3\) e \(4CaO·Al_2O_3·Fe_2O_3\) e o restante de \(CaO\) não combinado.</p>
 <div class="box-centro">
-    <p>\(2CaO·SiO_2 + CaO \rightarrow\) \(3CaO·SiO_2\)</p>
+    <div class="formula">
+        <div class="line">
+            <p>\(2CaO·SiO_2 + CaO \rightarrow\) \(3CaO·SiO_2\)</p>
+            <span class="box-eq">equação 3.8</span>
+        </div>
+    </div>
 </div>
 
 <p><span class="bold">VII. Primeiro resfriamento: </span>o primeiro resfriamento ocorre dentro do forno, após o clínquer passar pela zona de máxima temperatura. Nesta etapa pode ocorrer a decomposição do \(3CaO·SiO_2\) segundo a reação:</p>
 <div class="box-centro">
-    <p>\(3CaO·SiO_2 \rightarrow 2CaO·SiO_2\) \( +CaO\)</p>
+    <div class="formula">
+        <div class="line">
+            <p>\(3CaO·SiO_2 \rightarrow 2CaO·SiO_2\) \( +CaO\)</p>
+            <span class="box-eq">equação 3.9</span>
+        </div>
+    </div>
 </div>
 <div class="box">
     <p>A complementação das reações de clinquerização pode ser afetada pelo resfriamento sofrido pelo clínquer. Um resfriamento lento leva a um cimento de baixa qualidade.</p>
@@ -158,8 +205,9 @@ include("components/page-header.php");
 
 <p><span class="bold">VIII. Segundo resfriamento: </span>o segundo resfriamento ocorre abaixo de 1200 °C, já no resfriador. Este resfriamento lento também provoca uma maior corrosão dos cristais de \(3CaO·SiO_2\) pela penetração desta fase, nas bordas dos cristais, auxiliando a formação de \(2CaO·SiO_2\). O magnésio não combinado terá sua cristalização nesta etapa. Quanto mais lento for o resfriamento, maior será o desenvolvimento dos cristais de \(MgO\), aglutinando em zonas.</p>
 
-<br><div class="atv">
-    <h3 class = "atividade">Atividade 3.2. </h3>
+<br>
+<div class="atv">
+    <h3 class="atividade">Atividade 3.2. </h3>
     <p>Fazer um fluxograma simplificado contendo os principais processos envolvidos na produção do cimento.</p>
 </div>
 
